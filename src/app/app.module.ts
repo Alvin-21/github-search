079fa779f264380478b089bb98806b2d3be72c8a
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-06 16:52:19 +0300

    Delete component for account details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { AccountDetailsComponent } from './account-details/account-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent,
-    AccountDetailsComponent
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
