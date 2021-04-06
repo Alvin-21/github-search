6b17eb40dc3732a68fbbf85e88f335efb69b5985
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-06 16:05:47 +0300

    Add component for displaying account details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    AccountDetailsComponent
   ],
   imports: [
     BrowserModule,
