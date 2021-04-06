4ff6a3f57455d88b9683c0b7acf3da120ba3a42d
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-06 14:58:41 +0300

    Add functions for getting user details, repo details and updating username.

--- src/app/search-service/search-engine.service.ts
+++ src/app/search-service/search-engine.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchEngineService {
 
-  constructor() { }
+  private username!: string;
+  private apiKey = environment.apikey;
+
+  constructor(private http: HttpClient) { }
+
+  userDetails(username: string) {
+    return this.http.get("https://api.github.com/users/" + username + "?access_token=" + this.apiKey);
+  }
+
+  updateAccount(username: string) {
+    this.username = username;
+  }
+
+  repoDetails(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos" + "?access_token=" + this.apiKey);
+  }
 }
