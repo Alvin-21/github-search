cc2c272110e777ce61cf0fde75f481cdd16aa418
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-07 07:56:21 +0300

    Add logic for hover directive.

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {}
 
+  @HostListener('mouseover') onHover() {
+    this.boxShadow("0 0 10px white");
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.boxShadow("0 0 0");
+  }
+
+  private boxShadow(value: string) {
+    this.elem.nativeElement.style.boxShadow = value;
+  }
 }
