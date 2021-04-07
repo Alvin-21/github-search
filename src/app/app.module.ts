ea633684fe9b5056613e32c2057dfa0b40553280
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-07 07:17:29 +0300

    Add pipe for calculating time.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
